5181e8ce9c795f9a2fcad628cde505505ecfeee7
tukky-14 <bigoonage0104@gmail.com>
2024-03-10 11:28:16 +0900

    zodを使用するソースに変更

--- src/app/form-page/page.tsx
+++ src/app/form-page/page.tsx
@@ -1,9 +1,17 @@
 'use client';
+import { zodResolver } from '@hookform/resolvers/zod';
 import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import * as z from 'zod';
+
+const schema = z.object({
+    name: z.string().min(1, { message: '必須入力です。' }).max(10, { message: 'Too long' }),
+    age: z.string().min(1, { message: '必須入力です。' }),
+});
 
 type FormData = {
     name: string;
+    age: number;
 };
 
 const FormPage: React.FC = () => {
@@ -11,7 +19,7 @@ const FormPage: React.FC = () => {
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm<FormData>();
+    } = useForm<FormData>({ resolver: zodResolver(schema) });
 
     const onSubmit: SubmitHandler<FormData> = (data) => {
         console.log(data);
@@ -20,14 +28,26 @@ const FormPage: React.FC = () => {
     return (
         <form className="p-4" onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-2">
-                <label htmlFor="name">Name</label>
+                <label htmlFor="name">名前</label>
                 <input
                     className="ml-2 inline-block rounded border"
                     id="name"
-                    {...register('name', { required: true })}
+                    {...register('name')}
                 />
                 {errors.name && (
-                    <span className="ml-1 inline-block text-red-600">This field is required</span>
+                    <span className="ml-1 inline-block text-red-600">{errors.name?.message}</span>
+                )}
+            </div>
+            <div className="mb-2">
+                <label htmlFor="age">年齢</label>
+                <input
+                    type="number"
+                    className="ml-2 inline-block rounded border"
+                    id="age"
+                    {...register('age')}
+                />
+                {errors.age && (
+                    <span className="ml-1 inline-block text-red-600">{errors.age?.message}</span>
                 )}
             </div>
             <button
